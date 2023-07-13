44d9257471a1967a8820f037ec8206db9ffc04db
Thiritin <me@thiritin.com>
2023-07-14 00:06:08 +0200

    add favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <!-- Favicon -->
+        <link rel="icon" type="image/svg" href="/favicon.svg">
+
         <title inertia>{{ config('app.name', 'Eurofurence Stream') }}</title>
         <!-- Scripts -->
         @routes
