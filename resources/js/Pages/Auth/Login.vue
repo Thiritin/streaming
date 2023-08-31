f97c468d739690abbcbbe7f18cb029a6191641f0
Thiritin <me@thiritin.com>
2023-08-31 04:04:11 +0200

    update wording

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -11,8 +11,8 @@ import LoginScreenWelcome from "@/Components/LoginScreenWelcome.vue";
             <title>Login</title>
         </Head>
         <Logo></Logo>
-        <LoginScreenWelcome title="Livestream" sub-title="Only for attendees" class="mb-4"></LoginScreenWelcome>
-        <p class="text-primary-300 text-sm mb-3">Please note that a valid registration is required to access the Eurofurence livestream.</p>
+        <LoginScreenWelcome title="Livestream" sub-title="Registration Required" class="mb-4"></LoginScreenWelcome>
+        <p class="text-primary-300 text-sm mb-3 u">A verified Eurofurence Identity account is required, to access this page. <a class="underline" href="https://identity.eurofurence.org/auth/register" target="_blank">Don't have one? Create one here for free!</a></p>
         <a
                 class='py-3 mt-4 bg-primary-500 rounded-lg px-12 ml-auto text-white text-2xl mb-4 font-semibold focus:outline-none'
                 :href="route('auth.login')"
